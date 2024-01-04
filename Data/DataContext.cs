635ef315fe77ec963f853a548d9eb5484150732a
Karthick Selvam <karthickselvam3100@gmail.com>
2024-01-04 15:40:17 +0530

    Create DB

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -7,7 +7,7 @@ namespace API.Data
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-
+            Database.EnsureCreated();
         }
 
         public DbSet<VehicleData> Vehicles { get; set; }
